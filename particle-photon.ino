1cc7f77f09852f553197e36fff26d872cb3cc7d0
Jeremy Ellis <keyfreemusic@gmail.com>
2019-09-15 22:03:41 -0700

    Update particle-photon.ino

--- particle-photon.ino
+++ particle-photon.ino
@@ -1,15 +1,26 @@
+
 void setup() {
     pinMode(D7, OUTPUT);
-    Particle.function("myTogglePin", myTogglePinFunction);
+    Particle.function("doAll", doAllFunction);   
 }
 
 void loop() {
 
 }
 
-int myTogglePinFunction(String myCommand) {
-    digitalWrite(D7, !digitalRead(D7));
-    Particle.publish("myCommand variable is: ", myCommand, 60, PRIVATE);  
-    
-    return digitalRead(D7);
+int myReturn = 0;
+
+int doAllFunction(String myCommand) {
+    if  (myCommand == "toggleLED"){
+        digitalWrite(D7, !digitalRead(D7));
+        Particle.publish("myCommand variable is: ", myCommand, 60, PRIVATE);  
+      myReturn = digitalRead(D7);    // just so it is either 0 or 1
+
+    } else if (myCommand == "photoResistor"){
+        
+        myReturn = analogRead(A0);   
+    } else {
+        myReturn = -1;   // showing an incorrect argument
+    }
+    return myReturn;                                
 }
