c63c01f37d11c1368df6bd49939fe7f8fc3c26d8
Jeremy Ellis <keyfreemusic@gmail.com>
2019-09-15 22:02:54 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var request = require("request");
 
-let myParticleId = "f1234f1234"
+let myParticleId = "f1234f1234"     // you gotta enter this
+let myParticleFunction = "doAll"
 
 var options = {
-    url: "https://api.particle.io/v1/devices/"+myParticleId+"/my-main",
+    url: "https://api.particle.io/v1/devices/"+myParticleId+"/"+ myParticleFunction,
     method: "POST",
     form: {
-        access_token:"c43212c4321c4321",
+        access_token:"c43212c4321c4321",    // you gotta enter this
         arg:"SpecialStuff"
     }
 };
