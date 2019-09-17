15f426a7c347726878dcbedba95c76f9994c72bc
Jeremy Ellis <keyfreemusic@gmail.com>
2019-09-17 08:26:30 -0700

    Update particle-photon.ino

--- particle-photon.ino
+++ particle-photon.ino
@@ -10,15 +10,17 @@ void loop() {
 
 int myReturn = 0;
 
-int doAllFunction(String myCommand) {
-    if  (myCommand == "toggleLED"){
+int doAllFunction(String myParams) {
+    if  (myParams == "toggleLED"){
         digitalWrite(D7, !digitalRead(D7));
-        Particle.publish("myCommand variable is: ", myCommand, 60, PRIVATE);  
-      myReturn = digitalRead(D7);    // just so it is either 0 or 1
+        myReturn = digitalRead(D7);    // just so it is either 0 or 1
+        Particle.publish("Received Params: " + String(myParams), "Returned: " + String(myReturn), 60, PRIVATE);  
 
-    } else if (myCommand == "photoResistor"){
-        
+    } else 
+    
+    if (myParams == "photoResistor"){
         myReturn = analogRead(A0);   
+        Particle.publish("Received Params: " + String(myParams), "Returned: " + String(myReturn), 60, PRIVATE);  
     } else {
         myReturn = -1;   // showing an incorrect argument
     }
